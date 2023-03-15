6ab5c1a8adfc1ed7eb8317488f16f1d146755e82
Jonathan Lo <126200054+jolo988@users.noreply.github.com>
2023-03-14 21:46:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// read both color inputs (select both inputs)
-// add text with color values in css format (select h3)
-// by assign listerner event
-// query the background
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const random = document.getElementById("random")
 
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var random = document.getElementById("random")
-
-
+// setGradient: sets left (color1.value) and right (color2.value) of the background base on "input" DOM,
+// css.textContent: notifies user of hexadecimal color codes based on "input" DOM 
 function setGradient () {
 	body.style.background = 
 	"linear-gradient(to right, " 
@@ -21,49 +17,33 @@ function setGradient () {
 	css.textContent = body.style.background + ";"
 }
 
+// "input" DOM for user to choose 2 color gradients
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
 
-
+// randomColor generates random hexadecimal color code 
 function randomColor() {
-	var letters = "0123456789ABCDEF";
-	var newColor = "#";
+	let letters = "0123456789ABCDEF"; // contains hexadecimal color code + A to F
+	let newColor = "#"; // initializer
+
+	// randomly generated index choosen by randomly multiplying any number between 0-1 by 16, 
+	// then rounding it down with Math.floor 
+	// then looping 'letters' six times to return 'letters' by generated index
 	for (var i = 0; i < 6; i++) {
 		newColor += letters[Math.floor(Math.random()*16)];
 	}
 	return newColor
 }
 
-// random hexadecimal color code.
-
-// Here's a breakdown of how the function works:
-
-// The variable letters is defined as a string of all possible hexadecimal digits: "0123456789ABCDEF".
-// The variable color is initialized as the string "#" to represent the beginning of a hexadecimal color code.
-// A loop is started that runs six times, using the variable i as the loop counter.
-// Inside the loop, a random integer between 0 and 15 is generated using Math.random() * 16, rounded down to the nearest integer using Math.floor(), and used as an index into the letters string to select a random hexadecimal digit.
-// The selected digit is added to the color string, so that after six iterations of the loop, color contains a complete six-digit hexadecimal color code.
-// The final color string is returned as the result of the function.
-// Overall, this function generates a random color code by selecting six random hexadecimal digits and concatenating them together with a "#" character to form a valid color code string. The resulting color will be a random shade of red, green, or blue, since these are the primary colors used to create all other colors in the RGB color model.
-
-
-
+// Both color values call randomColor() for both color schemes, 
+// and call setGradient() to set random background gradient
 function randomGradient() {
 	color1.value = randomColor();
 	color2.value = randomColor();
 	setGradient();
 }
 
+// 'random' button randomly chooses color gradient when clicked by user, by calling randomGradient()
 random.addEventListener("click", randomGradient);
-
-
-// BONUS: Add a random button which generates 
-// two random numbers for the colour inputs.
-// assign button variable DONE
-// create random function (random math) DONE
-// create function for color1 and color2 to run DONE
-// click listen event (no text needed) DONE
-
 
 
